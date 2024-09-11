1175492a9d491a7a0885b4dd101b5a8cf4206e09
Santiago Salas <santiagosalas.6532@gmail.com>
2024-09-11 13:24:54 -0500

    Initial commit

---
+++ src/main/java/com/pragma/pragma_tecnologies/PragmaTecnologiesApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.pragma_tecnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragmaTecnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragmaTecnologiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/pragma_tecnologies/PragmaTecnologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.pragma_tecnologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PragmaTecnologiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
